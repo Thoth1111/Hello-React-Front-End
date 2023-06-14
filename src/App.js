a1e308ed2c16aad6660fde0f9215487847574cb8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-06-14 14:02:01 +0300

    Refactor index.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" component={Greeting} />
-      </Routes>
-    </Router>
+    <>
+      <Greeting />
+    </>
   );
 }
 
